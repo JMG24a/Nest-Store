445cd6b0b766b25587fe42abb51eaceadafca8a1
JMG24a <jmg24a@gmail.com>
2022-04-22 19:39:27 -0400

    [FIX]

--- src/common/parse-int.pipe.ts
+++ src/common/parse-int.pipe.ts
@@ -7,8 +7,8 @@ import {
 
 @Injectable()
 export class ParseInt implements PipeTransform {
-  transform(value: string, metadata: ArgumentMetadata) {
-    const val = Number(value); //or parseInt (value, 10)
+  transform(values: string, metadata: ArgumentMetadata) {
+    const val = Number(values); //or parseInt (value, 10)
     if (isNaN(val)) {
       throw new BadRequestException(`${value} is not an number`);
     }
