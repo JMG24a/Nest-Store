4d0bf7c2bf98f2b17022ee3a7e06b01dbdb43ad5
JMG24a <jmg24a@gmail.com>
2022-04-22 19:05:15 -0400

    app.module/service configuracion para cargar y validar las variables de entorno con Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,44 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpService, HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 //modules
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { firstValueFrom } from 'rxjs';
+import { DatabaseModule } from './database/database.module';
+import { environments } from './environments';
+import config from './config';
 
-const API_KEY_PROD = 1234;
-const API_KEY = [{ name: 'Jose' }];
+const configService = new ConfigService();
 
 @Module({
-  imports: [HttpModule, ProductsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        API: Joi.string().required(),
+      }),
+    }),
+    HttpModule,
+    ProductsModule,
+    UsersModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
     {
-      provide: 'Api_key',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-    {
       provide: 'Api_Json',
       useFactory: async (http: HttpService) => {
-        const response = await http.get(
-          `https://jsonplaceholder.typicode.com/todos`,
-        );
+        const response = await http.get(`${configService.get('API')}`);
         const tasks = await firstValueFrom(response);
         return tasks.data;
       },
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('Api_key') private apiKey: any,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
     @Inject('Api_Json') private tasks: any[],
   ) {}
 
   getHello(): string {
     console.log(this.tasks);
-    return 'Hello World!' + '' + this.apiKey[0].name;
+    return 'Hello World!' + ' ' + this.configService.apiKey;
   }
 }
