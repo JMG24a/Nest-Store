aedb630e0f23e8fc07cfe8a62ef557af28414e0e
JMG24a <jmg24a@gmail.com>
2022-04-23 12:16:57 -0400

    Deploy heroku

--- src/common/parse-int.pipe.ts
+++ src/common/parse-int.pipe.ts
@@ -8,7 +8,7 @@ import {
 @Injectable()
 export class ParseInt implements PipeTransform {
   transform(values: string, metadata: ArgumentMetadata) {
-    const val = Number(values); //or parseInt (value, 10)
+    const val = Number(values); //or parseInt (value, 10) para permitir valores 1sdsd5
     if (isNaN(val)) {
       throw new BadRequestException(`${values} is not an number`);
     }
