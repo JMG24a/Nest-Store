7aa9e04d227c72143e7497296d1b1bb61779c058
jmg24a <jmg24a@gmail.com>
2024-05-11 17:08:48 -0400

    [update] main.js -> docs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { documentation } from './environments.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,13 +12,7 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('Nest Store')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  documentation(app)
 
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
