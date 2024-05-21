a8331dea3f7a4da7e01f2d0f7d888bf08e604dc6
jmg24a <jmg24a@gmail.com>
2024-05-21 16:54:16 -0400

    [update] module.database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,29 +1,12 @@
-import { HttpService, HttpModule } from '@nestjs/axios';
 import { Module, Global } from '@nestjs/common';
-
-const API_KEY_PROD = 'useValue';
+// my dependencies
+import { typeOrmModule } from './postgres.config';
 
 @Global()
 @Module({
-  imports: [HttpModule],
-  providers: [
-    {
-      provide: 'USE_VALUE',
-      useValue: API_KEY_PROD
-    },
-    {
-      provide: 'TASK',
-      useFactory: async (http: HttpService) => {
-        const request = await http.get('https://jsonplaceholder.typicode.com/todos')
-        return new Promise((resolve)=>{
-          request.subscribe((response)=>{
-            resolve(response.data);
-          })
-        })
-      },
-      inject: [HttpService]
-    }
-  ],
-  exports: ['TASK', 'USE_VALUE'],
+  imports: [typeOrmModule],
+  controllers: [],
+  providers: [],
+  exports: [typeOrmModule],
 })
 export class DataBaseModule {}
